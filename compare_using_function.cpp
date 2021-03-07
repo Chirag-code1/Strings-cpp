0ba168bdb78dfee477dd21506c819cdca5b82269
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-07 23:49:18 +0530

    Add files via upload

---
+++ compare_using_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+#include <cstring>  
+using namespace std;  
+int main ()  
+{  
+  char s1[] = "cricket";  
+  char s2[50];  
+  do {  
+     cout<<"What is my favourite sport? ";  
+     cin>>buffer;  
+  } while (strcmp (s1,s2) != 0);  
+ cout<<"Answer is correct!!"<<endl;  
+  return 0;  
+}  
