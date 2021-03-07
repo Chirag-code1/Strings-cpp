1567b75afaa3e235cfeea820f1d8390fee801ee3
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-07 23:49:32 +0530

    Add files via upload

---
+++ concat_using_function.cpp
@@ -0,0 +1,18 @@
+
+//Note:-> Don't forget to include "#include <cstring>"
+
+#include <iostream>  
+#include <cstring>  
+using namespace std;  
+int main()  
+{  
+    char s1[25], s2[25];  
+    cout << "Enter the s1 string: ";  
+    cin.getline(s1, 25);  
+    cout << "Enter the s2 string: ";  
+     cin.getline(s2, 25);  
+    strcat(s1, s2);   
+    cout << "S1 = " << s1 << endl;  
+    cout << "S2 = " << s2<<endl;  
+    return 0;  
+}
