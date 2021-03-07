c999bd5e3c65626894780c4cebae317f86c499e5
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-07 23:55:22 +0530

    Add files via upload

---
+++ String_insert-using_function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  
+#include<cstring>
+using namespace std;  
+int main()  
+  {
+string str1= "I love shakes!";  
+cout<<"String contains :" <<str1<<'\n';  
+cout<<"After insertion, String value is :"<<str1.insert(7,"icecreams and ");  
+return 0;  
+} 
