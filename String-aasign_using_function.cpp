b1ae65f3efbef35b85dbbae49288d35c828119ab
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-07 23:56:58 +0530

    Add files via upload

---
+++ String-aasign_using_function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  
+using namespace std;  
+int main()  
+{  
+string str = "Chirag is a peaceful soul";  
+string str1;  
+str1.assign(str);  
+cout<<"Assigned string is : " <<str1;  
+return 0;  
+} 
