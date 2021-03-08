4681f04810744d2b59e512278bcf42b483e2fba8
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-08 23:40:27 +0530

    Add files via upload

---
+++ str.pop_back.cpp
@@ -0,0 +1,27 @@
+ 
+#include<iostream> 
+#include<string> // for string class 
+using namespace std; 
+int main() 
+{ 
+	// Declaring string 
+	string str; 
+	getline(cin,str); 
+
+	// Displaying string 
+	cout << "The initial string is : "; 
+	cout << str << endl; 
+
+
+
+	// Using pop_back() to delete a character 
+	// from end 
+	str.pop_back(); 
+
+	// Displaying string 
+	cout << "The string after pop_back operation is : "; 
+	cout << str << endl; 
+
+	return 0; 
+
+} 
