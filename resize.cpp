37cc3484b89f3c45a06cdf4d371693c589c7115a
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-08 23:47:27 +0530

    Add files via upload

---
+++ resize.cpp
@@ -0,0 +1,26 @@
+// C++ code to demonstrate the working of 
+// resize()
+#include<iostream> 
+#include<string> // for string class 
+using namespace std; 
+int main() 
+{ 
+	// Initializing string 
+	string str = "MAHATMA GANDHI WAS GREAT"; 
+
+	// Displaying string 
+	cout << "The initial string is : "; 
+	cout << str << endl; 
+
+	// Resizing string using resize() 
+	str.resize(13); 
+
+	// Displaying string 
+	cout << "The string after resize operation is : "; 
+	cout << str << endl; 
+
+	
+
+	return 0; 
+
+} 
