4b69d66254b12086a7f49053fae9a33f4ec088c1
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-10 23:44:39 +0530

    Add files via upload

---
+++ String copy().cpp
@@ -0,0 +1,11 @@
+#include<iostream>  
+using namespace std;  
+int main()  
+{  
+string source = "GandhiJi was Great!";  
+char destination[20];  
+cout<<"source string is : "<<source<<'\n';  
+source.copy(destination,sizeof source);   
+cout<<"destination string is :"<<destination;  
+return 0;  
+} 
