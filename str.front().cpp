19097ff27ad4d4f1a1fd12ad4721c8c6628a57c4
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-10 23:47:44 +0530

    Add files via upload

---
+++ str.front().cpp
@@ -0,0 +1,11 @@
+//It is used to return a reference of the first character.
+
+#include<iostream>  
+#include<string>  
+using namespace std;  
+int main()  
+{  
+    string str ="12345";  
+    cout<<str.front();  
+    return 0;  
+}
