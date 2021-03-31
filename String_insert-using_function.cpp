eb908ea1ca9f5d4204aa529097e428a64ddaf037
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-31 22:23:23 +0530

    Update String_insert-using_function.cpp

--- String_insert-using_function.cpp
+++ String_insert-using_function.cpp
@@ -5,6 +5,7 @@ int main()
   {
 string str1= "I love shakes!";  
 cout<<"String contains :" <<str1<<'\n';  
-cout<<"After insertion, String value is :"<<str1.insert(7,"icecreams and ");  
+cout<<"After insertion, String value is :"<<str1.insert(7,"icecreams and ");
+  
 return 0;  
 } 
